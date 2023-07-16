4b3ca6b9e02e89ac87fa42ebdb6d6b63237de517
ilya <ilasafronov96@gmail.com>
2023-07-16 17:44:25 +0300

    refactored

--- main.py
+++ main.py
@@ -5,28 +5,17 @@ from datetime import datetime
 from Loader import Loader
 from units.LorenzPoint import LorenzPoint
 from units.RosselPoint import RosselPoint
-<<<<<<< HEAD
 from units.NAttractorPoint import NAttractorPoint
-=======
->>>>>>> 9f426dff9e844b599c4d15ab7e67695a6798f5f5
 from colors import *
 pg.init()
 
 """constants"""
-<<<<<<< HEAD
-fps = 100000
-=======
-fps = 240
->>>>>>> 9f426dff9e844b599c4d15ab7e67695a6798f5f5
+fps = 5000
 time = 0
 screen = pg.display.set_mode((1000, 1000))
 screen.fill(BLACK)
 clock = pg.time.Clock()
-<<<<<<< HEAD
 loader = Loader("units/pattetns/rainbow_r.jpg")
-=======
-loader = Loader("units/pattetns/purpleorange.jpg")
->>>>>>> 9f426dff9e844b599c4d15ab7e67695a6798f5f5
 
 
 """Dinamic"""
@@ -40,13 +29,8 @@ while True:
     time += 1 / fps
 
     if len(points) <= 1000000:
-<<<<<<< HEAD
         #points[len(points) - 1].draw()
         np = points[len(points) - 1].getNext(1/60 )
-=======
-        points[len(points) - 1].draw()
-        np = points[len(points) - 1].getNext(1/fps )
->>>>>>> 9f426dff9e844b599c4d15ab7e67695a6798f5f5
         points.append(np)
         np.draw()
         #points.pop(0)
